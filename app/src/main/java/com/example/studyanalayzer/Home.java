cce0b4d114dcd0d0fc35639d0e0949deb253ead5
Omkar Durande <vanitadurande130@gmail.com>
2025-01-15 02:39:06 -0500

    home page with bottom navigation bar

--- app/src/main/java/com/example/studyanalayzer/Home.java
+++ app/src/main/java/com/example/studyanalayzer/Home.java
@@ -1,6 +1,8 @@
 package com.example.studyanalayzer;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,8 +19,15 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, 0, systemBars.right, 0);
             return insets;
+        });
+        ImageView home = findViewById(R.id.homeNav);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                home.setImageResource(R.drawable.homedark);
+            }
         });
 
     }
